ede8e03ba2bcbbd764aadfa1df579356f2bf8102
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-07-20 15:10:09 +0530

    new

--- 02_Creating_app/project/app/views.py
+++ 02_Creating_app/project/app/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def django_course(request):
+    return HttpResponse("<h1>Hello World</h1>")
+    
