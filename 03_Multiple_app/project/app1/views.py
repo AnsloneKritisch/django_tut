2171f163546d5c7bff212895c7ef6f3d18371cb0
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-07-25 09:51:34 +0530

    new

--- 03_Multiple_app/project/app1/views.py
+++ 03_Multiple_app/project/app1/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def django_app1(request):
+    return HttpResponse("Hello World From app1 ")
+
--- 03_Multiple_app/project/project/urls.py
+++ 03_Multiple_app/project/project/urls.py
@@ -1,21 +1,13 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from app1 import views as ap1
+from app2 import views as ap2
+from app3 import views as ap3
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dj_app1/',ap1.django_app1),
+    path('dj_app2/',ap2.django_app2),
+    path('dj_app3/',ap3.django_app3),
 ]
+
